0c6881712225933c1f6e31c478049e316aa0dd15
Vladyslav Chaban <dma.employee.v@gmail.com>
2023-05-14 21:17:42 +0300

    photo model

---
+++ src/database/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, func, ForeignKey, Boolean, Text
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.sqltypes import DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class Photo(Base):
+    __tablename__ = "photos"
+
+    id = Column(Integer, primary_key=True)
+    photo_url = Column(String(400))
+    qr_code_url = Column(Text)
+    title = Column(String(69), nullable=True)
+    description = Column(String(777), nullable=True)
+    #tags = ...
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now())
+
+    # user_id = Column('user_id', ForeignKey('users.id', ondelete='CASCADE'), default=None)
+    # user = relationship('User', backref="photos")
